54aa7025c4671449f71a4e2ccf8a58656feab8a3
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-09-07 10:42:31 -0300

    delete unused CustomScroll

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Gasto from './Gasto';
-import CustomScroll from 'react-custom-scroll';
 
 const Listado = ({gastos}) => (
     <div>
